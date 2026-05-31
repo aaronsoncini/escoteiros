69c885f9fd0e388fd1b3c19e3f6f0a02d1450f07
aaronsoncini <aaron.soncinie@gmail.com>
2026-05-31 18:19:59 -0300

    commit para banco de dados

--- database/database.js
+++ database/database.js
@@ -2,15 +2,16 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const connection = new Sequelize(
-    process.env.DB_NAME || 'escoteiros',
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mysql',
-        timezone: '-03:00',
-        logging: false
-    }
+  process.env.DB_NAME || 'escoteiros',
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // Garante que a porta do Railway seja usada
+    dialect: 'mysql',
+    timezone: '-03:00',
+    logging: false
+  }
 );
 
 module.exports = connection;
